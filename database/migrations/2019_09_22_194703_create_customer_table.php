6a8790f98c151881e7efcdf31227468a90968b60
Erik Boström <erikbostrom1@gmail.com>
2019-09-22 22:55:15 +0200

    add migration and seeder for customer

---
+++ database/migrations/2019_09_22_194703_create_customer_table.php
@@ -0,0 +1,40 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateCustomerTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('customer', function (Blueprint $table) {
+            $table->bigIncrements('id')->autoIncrement();
+            $table->integer('form_id')->nullable(false);
+            $table->string('first_name')->nullable(false);
+            $table->string('last_name')->nullable(false);
+            $table->string('email')->nullable(false);
+            $table->string('telephone_number')->nullable();
+            $table->string('real_eastate_type')->nullable();
+            $table->integer('bedrooms')->nullable();
+            $table->integer('bathrooms')->nullable();
+            $table->timestamp('submit_date')->nullable();
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('customer');
+    }
+}
